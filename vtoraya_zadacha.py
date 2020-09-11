7b173d0e1c30469883299558addc3902320b7aab
pin034 <60174760+pin034@users.noreply.github.com>
2020-09-11 12:50:56 +0700

    Update vtoraya_zadacha.py

--- vtoraya_zadacha.py
+++ vtoraya_zadacha.py
@@ -1,9 +1,16 @@
+d = 0
 print("начальное время")
 CH, M, S = map(int, input().split())
 print("конечное время")
 chk, mk, sk = map(int, input().split())
 razn = (chk * 3600 + mk * 60 + sk) - (CH * 3600 + M * 60 + S)
-cho = razn // 3600
-mo = (razn % 3600) // 60
-so = razn % 60 
+if razn > d:
+    cho = razn // 3600
+    mo = (razn % 3600) // 60
+    so = razn % 60 
+else:
+    razn = razn + 24 * 3600
+    cho = razn // 3600
+    mo = (razn % 3600) // 60
+    so = razn % 60 
 print(cho," ", mo," ", so)
